826843c77e9b3f7679efa4d40dff937e96be3115
anonymous anonymous <noelle.harasym@gmail.com>
2022-03-13 17:40:12 +0000

    Remove controller comments

--- app/controllers/sweetnesses_controller.rb
+++ app/controllers/sweetnesses_controller.rb
@@ -1,26 +1,21 @@
 class SweetnessesController < ApplicationController
   before_action :set_sweetness, only: %i[show edit update destroy]
 
-  # GET /sweetnesses
   def index
     @q = Sweetness.ransack(params[:q])
     @sweetnesses = @q.result(distinct: true).includes(:coffee_orders).page(params[:page]).per(10)
   end
 
-  # GET /sweetnesses/1
   def show
     @coffee_order = CoffeeOrder.new
   end
 
-  # GET /sweetnesses/new
   def new
     @sweetness = Sweetness.new
   end
 
-  # GET /sweetnesses/1/edit
   def edit; end
 
-  # POST /sweetnesses
   def create
     @sweetness = Sweetness.new(sweetness_params)
 
@@ -31,7 +26,6 @@ class SweetnessesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /sweetnesses/1
   def update
     if @sweetness.update(sweetness_params)
       redirect_to @sweetness, notice: "Sweetness was successfully updated."
@@ -40,7 +34,6 @@ class SweetnessesController < ApplicationController
     end
   end
 
-  # DELETE /sweetnesses/1
   def destroy
     @sweetness.destroy
     redirect_to sweetnesses_url, notice: "Sweetness was successfully destroyed."
@@ -48,12 +41,10 @@ class SweetnessesController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_sweetness
     @sweetness = Sweetness.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def sweetness_params
     params.require(:sweetness).permit(:sweetness_type)
   end
