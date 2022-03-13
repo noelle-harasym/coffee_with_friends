f50112f4b4927b4de3cbb9180f415fce9859452c
anonymous anonymous <noelle.harasym@gmail.com>
2022-03-13 17:38:37 +0000

    Generate coffee_orders resource with ideas:scaffold

---
+++ db/migrate/20220313173836_create_coffee_orders.rb
@@ -0,0 +1,17 @@
+class CreateCoffeeOrders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :coffee_orders do |t|
+      t.integer :user_id
+      t.integer :coffee_type_id
+      t.boolean :modification_iced
+      t.integer :dairy_type_id
+      t.integer :sweetness_type_id
+      t.text :modification_other
+      t.integer :rating
+      t.string :picture
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
